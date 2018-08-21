69ecf7d0bfcc420fe415ec03f4788ab74f251938
James Chong <wei3hua2@gmail.com>
2018-08-21 12:20:25 +0800

    curry request

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import requestPromise from 'request-promise';
+import request from 'request';
 import {RpsContext,RpsModule,rpsAction} from 'rpscript-interface';
 
-/** Request Module
- * @namespace Request 
+/** Module for making http call
+ * @see {@link https://www.npmjs.com/package/request-promise}
+ * @namespace Request
+ * 
+ * @example
+ * rps install request
 */
 @RpsModule("request")
 export default class RPSRequest {
 
+/**
+ * @function request
+ * @memberof Request
+ * @example
+ * ; GET the content of google.com
+ * request "http://www.google.com"
+ *
+ * @param {string} url url
+ * @param {*} options refer to request-promise documentation.
+ * @summary request :: String → String
+ * 
+ * @see {@link https://www.npmjs.com/package/request-promise}
+ * 
+*/
   @rpsAction({verbName:'request'})
-  async request (ctx:RpsContext,opts:Object, url:any) : Promise<any>{
-    return requestPromise(url);
+  async request (ctx:RpsContext,opts:any, url?:string) : Promise<any|Function>{
+    function req(url) {
+      opts['uri'] = url;
+      return requestPromise(opts);
+    }
+
+    if(!url) return req;
+    else return req(url);
   }
   
 }
--- test/index.ts
+++ test/index.ts
@@ -8,10 +8,18 @@ m.describe('Request', () => {
 
   m.it('should extract data', async function () {
     let req = new RPSRequest;
+    let url = "https://gist.githubusercontent.com/wei3hua2/36112a3ebcc0d92ebb2f7b2753ae41ac/raw/67eff3f7c81c74b4e25ab2e5d557b91e748c3c4c/hello.rps";
 
-    let output = await req.request(new RpsContext,{},"http://www.google.com.sg");
+    let output = await req.request(new RpsContext,{},url);
     
     console.log(output);
+
+    let fn = await req.request(new RpsContext,{});
+    let result = await fn(url);
+
+    console.log(result);
+
+    expect(output).to.be.equals(result);
 
   }).timeout(0);
 
